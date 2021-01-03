727ec784581d2fb2844750eb1cc54818680ab54f
Lexi C <cummins.a88@gmail.com>
2021-01-03 14:17:30 -0500

    first commit

---
+++ source_files/source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char myCharacter;
+    myCharacter = 'y';
+
+    int myInt;
+    myInt = 13;
+
+    std::cout << myCharacter << endl;
+
+    system("pause");
+}
