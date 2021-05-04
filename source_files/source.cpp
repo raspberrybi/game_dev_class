d30a6d11e886b01fe3dfd3ef3cb42cea5fc6703b
Lexi C <cummins.a88@gmail.com>
2021-05-04 13:44:17 -0400

    control flow practice

--- source_files/source.cpp
+++ source_files/source.cpp
@@ -8,7 +8,15 @@ int main()
 
     if (b < a)
     {
+        cout << "b is less than a." << endl;
+    }
+    else if (a < b)
+    {
         cout << "a is less than b." << endl;
+    }
+    else
+    {
+        cout << "a is equal to b." << endl;
     }
 
     system("pause");
